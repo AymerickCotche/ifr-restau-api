5fddd1a8309a0df049758a2a784e8e12e736942a
AymerickCotche <aymerick.cotche@hotmail.fr>
2023-11-15 15:29:37 +0400

    adding api route

--- app/api/registre/getallperetablissement/route.ts
+++ app/api/registre/getallperetablissement/route.ts
@@ -6,6 +6,7 @@ export async function GET(request:NextRequest){
   try {
 
     const searchParams = request.nextUrl.searchParams
+    
     const etablissementId = searchParams.get('etablissementId')
 
     if (etablissementId) {
